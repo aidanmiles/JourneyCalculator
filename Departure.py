da94a57581fd483575de4bc5efd38bcc84f96ac1
Aidan Miles <37910055+aidanmiles@users.noreply.github.com>
2020-11-24 01:02:19 +0000

    Update Departure.py
    Comments

--- Departure.py
+++ Departure.py
@@ -2,7 +2,7 @@
 graph = {'A': {'B': 800}, 'B': {'C': 900, 'F': 400}, 'C': {'D': 400, 'E': 300, 'E': 200},
          'D': {'C': 700, 'E': 400, 'E': 300}, 'E': {'B': 600, 'B': 500}, 'F': {'D': 200}}
 
-
+# path is set to 0 as well as the nodes to ensure route is started from the start node
 def dijkstra(graph, start, goal, passengers):
     shortest_distance = {}
     predecessor = {}
@@ -20,13 +20,14 @@ def dijkstra(graph, start, goal, passengers):
                 minNode = node
             elif shortest_distance[node] < shortest_distance[minNode]:
                 minNode = node
+        #calculating the lowest weight thus the shortest path
 
         for childNode, weight in graph[minNode].items():
             if weight + shortest_distance[minNode] < shortest_distance[childNode]:
                 shortest_distance[childNode] = weight + shortest_distance[minNode]
                 predecessor[childNode] = minNode
         unseenNodes.pop(minNode)
-
+    #error handling to catch any unacceptable input 
     currentNode = goal
     while currentNode != start:
         try:
